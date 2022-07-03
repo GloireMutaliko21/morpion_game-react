22f87a1b52cc05feccfaf27cd1f30ddf5f75221f
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-03 21:23:00 +0200

    feat: trouver le gagnant

--- src/index.js
+++ src/index.js
@@ -41,8 +41,16 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Prochain joueur : ' +
-            (this.state.xIsNext ? 'X' : 'O');
+
+        const winner = calculateWinner(this.state.squares)
+        let status
+        if (winner) {
+            status = winner + ' a gagné';
+        } else {
+            status = 'Prochain joueur : ' +
+                (this.state.xIsNext ? 'X' : 'O');
+        }
+
 
         return (
             <div>
@@ -81,6 +89,26 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
