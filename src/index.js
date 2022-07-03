2dbaceabdec070fa0bb9593d58bedd2643033804
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-03 21:27:43 +0200

    feat: bloquer le clic après avoir trouvé le gagnant

--- src/index.js
+++ src/index.js
@@ -24,6 +24,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
