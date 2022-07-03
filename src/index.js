fb038a0b76e65054b65d7eb280ed8d6302b9fca8
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-03 21:10:06 +0200

    feat: jouer tour à tour

--- src/index.js
+++ src/index.js
@@ -17,14 +17,18 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
@@ -37,7 +41,8 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Prochain joueur : ' +
+            (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
